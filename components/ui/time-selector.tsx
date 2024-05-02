069a9d53adf8d820baccfe236cc833e5a5e916eb
JoarM <joar.maltesson@gmail.com>
2024-05-02 21:17:02 +0200

    refactor

--- components/ui/time-selector.tsx
+++ components/ui/time-selector.tsx
@@ -18,14 +18,6 @@ function TimeSelector({
   const [isMouseDown, setIsMouseDown] = useState(false);
   const [time, setTime] = useState(new Date());
   const clockHandle = useRef<HTMLDivElement>(null);
-  
-  function calculateAngle(length: number, height: number) {
-    return Math.atan2(height, length) * 57.2957795 < 0 ? Math.atan2(height, length) * 57.2957795 + 360 : Math.atan2(height, length) * 57.2957795;
-  }
-
-  function pythagoras(a: number, b: number) {
-    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-  }
 
   function setHandleToHourPosition() {
     const hours = time.getHours();
---
+++ lib/math.ts
@@ -0,0 +1,7 @@
+function calculateAngle(length: number, height: number) {
+  return Math.atan2(height, length) * 57.2957795 < 0 ? Math.atan2(height, length) * 57.2957795 + 360 : Math.atan2(height, length) * 57.2957795;
+}
+
+function pythagoras(a: number, b: number) {
+  return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+}
